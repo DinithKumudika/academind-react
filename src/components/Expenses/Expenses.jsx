e18eeeadb605fc96c4a5a216c2fc70c9af758360
DinithKumudika <dinithwalpitagama@gmail.com>
2023-04-10 13:27:28 +0530

    display expenses through array map

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -3,25 +3,17 @@ import ExpenseItem from './ExpenseItem/ExpenseItem';
 import Card from '../UI/Card/Card';
 import './Expenses.css';
 
-const Expenses = (props) => {
+const Expenses = ({items}) => {
      return (
           <Card className="expenses">
-               <ExpenseItem
-                    date={props.items[0].date}
-                    title={props.items[0].title}
-                    amount={props.items[0].amount} />
-               <ExpenseItem
-                    date={props.items[1].date}
-                    title={props.items[1].title}
-                    amount={props.items[1].amount} />
-               <ExpenseItem
-                    date={props.items[2].date}
-                    title={props.items[2].title}
-                    amount={props.items[2].amount} />
-               <ExpenseItem
-                    date={props.items[3].date}
-                    title={props.items[3].title}
-                    amount={props.items[3].amount} />
+               {items.map((expense) => (
+                    <ExpenseItem
+                         date={expense.date}
+                         title={expense.title}
+                         amount={expense.amount}
+                         key={expense.id}
+                    />
+               ))}
           </Card>
      )
 }
